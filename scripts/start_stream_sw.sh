1ccc6762a8e7c6ed4df70b7dcf32db5e22aa4d25
reverseiwnl <87674411+reverseiwnl@users.noreply.github.com>
2026-02-27 15:11:33 -0600

    Change video resolution for camera streams

--- scripts/start_stream_sw.sh
+++ scripts/start_stream_sw.sh
@@ -15,20 +15,22 @@ killall gst-launch-1.0 2>/dev/null
 # Camera 1 (Eye Left): /dev/video2
 echo "Starting Eye Camera 1 (video2) -> $CLIENT_IP:5000"
 gst-launch-1.0 -v v4l2src device=/dev/video2 ! \
-  image/jpeg,width=400,height=400,framerate=60/1 ! \
+  image/jpeg,width=192,height=192 ! \
   jpegdec ! \
   videoconvert ! \
-  x264enc tune=zerolatency speed-preset=ultrafast bitrate=2000 ! \
+  v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! \
+  'video/x-h264,level=(string)4' ! \
   rtph264pay config-interval=1 pt=96 ! \
   udpsink host=$CLIENT_IP port=5000 sync=false &
 
 # Camera 2 (Eye Right): /dev/video4
 echo "Starting Eye Camera 2 (video4) -> $CLIENT_IP:5001"
 gst-launch-1.0 -v v4l2src device=/dev/video4 ! \
-  image/jpeg,width=400,height=400,framerate=60/1 ! \
+  image/jpeg,width=192,height=192 ! \
   jpegdec ! \
   videoconvert ! \
-  x264enc tune=zerolatency speed-preset=ultrafast bitrate=2000 ! \
+  v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! \
+  'video/x-h264,level=(string)4' ! \
   rtph264pay config-interval=1 pt=96 ! \
   udpsink host=$CLIENT_IP port=5001 sync=false &
 
